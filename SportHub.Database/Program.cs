feab325017f1831115c27f9d52c803f69d79a386
Maxim Orlianskyi <orlianskyimaxim@gmail.com>
2023-04-13 10:24:49 +0300

    Add DB console runner C# script

--- SportHub.Database/Program.cs
+++ SportHub.Database/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.IO;
+namespace SQLScriptRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "SportHub";
+            string uid = "user";
+            string password = "root";
+            string scriptFilePath = @"CreateDB_script.sql";
+            string connectionString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            try
+            {
+                string script = File.ReadAllText(scriptFilePath);
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand(script, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error creating database: " + ex.Message);
+            }
+        }
+    }
+}
