3ae673f2a1f4ce09a6ddd3fa6cd420b8095c7d02
OndoriSp <ondori_special@ukr.net>
2023-04-20 20:43:33 +0300

    Backend for languages management

---
+++ SportHub.Business/Interfaces/ILanguageService.cs
@@ -0,0 +1,14 @@
+﻿using SportHub.Data.Entities;
+using SportHub.Data.DTO;
+
+namespace SportHub.Business.Interfaces
+{
+    public interface ILanguageService
+    {
+        Task<IEnumerable<LanguageResponse>> GetLanguagesAsync();
+        Task<Language> GetLanguageByTitleAsync(string shortTitle);
+        Task AddLanguageAsync(LanguageRequest languageDTO);
+        Task ChangeLanguageIsActiveAsync(string shortTitle, bool isActive);
+        Task DeleteLanguageAsync(string shortTitle);
+    }
+}
