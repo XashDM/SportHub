7e1e18c7f3d54425acc2444599379335e15fc2be
Oleh-Kr <mayorford777@gmail.com>
2023-04-20 08:53:30 +0300

    add google auth controller

---
+++ SportHub.API/Controllers/GoogleAuthorizationController.cs
@@ -0,0 +1,82 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SportHub.Business;
+using SportHub.Data.DTO;
+using SportHub.Data.Entities;
+
+namespace SportHub.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class GoogleAuthorizationController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+        private readonly IJwtService _jwtService;
+
+        public GoogleAuthorizationController(IHttpClientFactory httpClientFactory, IConfiguration config, 
+            IUserService userService, IMapper mapper, IJwtService jwtService)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _clientId = config.GetSection("GoogleCredentials")["ClientId"];
+            _clientSecret = config.GetSection("GoogleCredentials")["ClientSecret"];
+            _userService = userService;
+            _mapper = mapper;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AuthUser(string accessToken)
+        {
+            try
+            {
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+                // Send the request to the Google userinfo API
+                var response = await _httpClient.GetAsync("https://www.googleapis.com/oauth2/v2/userinfo");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    // Deserialize the JSON response into an UserGoogleDto
+                    UserGoogleDto userInfo = JsonConvert.DeserializeObject<UserGoogleDto>(content);
+
+                    var userFromDb = await _userService.GetUserByEmailAsync(userInfo.Email);
+                    
+                    if (userFromDb == null)
+                    {
+                        var user = _mapper.Map<UserGoogleDto, User>(userInfo);
+                        await _userService.InsertOneAsync(_mapper.Map<User, UserRequestDto>(user));
+                        userFromDb = await _userService.GetUserByEmailAsync(user.Email);
+                        await _userService.ActivateUserAccountAsync(userFromDb.UserId);
+                    }
+
+                    var tokens = await _jwtService.GenerateTokensAsync(userFromDb);
+                        
+                    return Ok(tokens);
+                }
+                else
+                {
+                    return BadRequest(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+    
+}
+
+
--- SportHub.API/MapperConfig.cs
+++ SportHub.API/MapperConfig.cs
@@ -9,8 +9,15 @@ namespace SportHub.API
         public MapperConfig()
         {
             CreateMap<User, UserResponseDto>();
+            CreateMap<User, UserRequestDto>();
             CreateMap<UserRequestDto, User>()
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<UserGoogleDto, User>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.FirstName,
+                    opt => opt.MapFrom(src => src.Name.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]))
+                .ForMember(dest => dest.LastName,
+                    opt => opt.MapFrom(src => src.Name.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]));
         }
     }
 }
