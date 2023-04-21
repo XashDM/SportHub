0cdaadcd0febb32f79cf3cfbf7752bf5d23b13cb
Oleh-Kr <mayorford777@gmail.com>
2023-04-21 09:05:55 +0300

    add success email sent page

--- SportHub/src/modules/ForgotPasswordForm/components/ForgotPasswordForm.js
+++ SportHub/src/modules/ForgotPasswordForm/components/ForgotPasswordForm.js
@@ -4,15 +4,17 @@ import Button from "../../../ui/Button"
 import {useState} from "react"
 import {useNavigate} from 'react-router-dom'
 import {ROUTES} from "../../../routes/routes"
+import EmailSentContainer from "../../../components/EmailSentContainer"
 
 
 function ForgotPasswordForm(){
     const [email, setEmail] = useState('')
     const [error, setError] = useState(false)
+    const [isSent, setIsSent] = useState(false)
     const navigate = useNavigate()
     const handleRequestPasswordButton = async (event) => {
         event.preventDefault()
-
+        setIsSent(true)
         // const result = await passwordResetRequest(email, password)
         //
         // if (result === "ERR_BAD_REQUEST") {
@@ -25,23 +27,30 @@ function ForgotPasswordForm(){
         // }
     }
 
+
     return(
-        <div className={styles.container}>
 
-            <div>
-                <h2 className={styles.heading}>Forgot your password?</h2>
-                {error && <h3 className={styles.error}>Incorrect email. Try again</h3>}
+        !isSent ?
+            <div className={styles.container}>
+                <div>
+                    <h2 className={styles.heading}>Forgot your password?</h2>
+                    {error && <h3 className={styles.error}>Incorrect email. Try again</h3>}
+                </div>
+
+                <p className={styles.text_muted}>Enter your email address below and we'll get you back on track.</p>
+
+                <Input label={"EMAIL ADRESS"}
+                placeholder={"Email@gmail.com"}
+                error={error}
+                onChange={(event) => setEmail(event.target.value)}/>
+
+                <Button onClick={handleRequestPasswordButton} text={"REQUEST RESET LINK"}/>
+                <button className={styles.bottom_link} onClick={() => navigate(ROUTES.LOGIN)}>Back to Log In</button>
             </div>
-            <p className={styles.text_muted}>Enter your email address below and we'll get you back on track.</p>
-
-            <Input label={"EMAIL ADRESS"}
-                   placeholder={"Email@gmail.com"}
-                   error={error}
-                   onChange={(event) => setEmail(event.target.value)}/>
-
-            <Button onClick={() => console.log("clicked")} text={"REQUEST RESET LINK"}/>
-            <button className={styles.bottom_link} onClick={() => navigate(ROUTES.LOGIN)}>Back to Log In</button>
-        </div>
+     :
+            <EmailSentContainer
+                heading={`Check your email ${email}`}
+                sub_heading={"If there's Sports Hub account linked to this email address, we'll send over instructions to reset your password."}/>
     )
 }
 
