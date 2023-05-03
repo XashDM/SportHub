116b9146cc54566cd40b7e9ed259e382844f46e3
Oleh-Kr <mayorford777@gmail.com>
2023-05-03 15:45:37 +0300

    Edit CreateDB file to be compatible with log-in-sign-up API

--- SportHub.Database/Scripts/CreateDB.sql
+++ SportHub.Database/Scripts/CreateDB.sql
@@ -1,19 +1,36 @@
 CREATE SCHEMA IF NOT EXISTS `SportHub` DEFAULT CHARACTER SET utf8mb4 ;
 USE `SportHub` ;
 
-CREATE TABLE IF NOT EXISTS `SportHub`.`UserAccount` (
-  `UserId` INT NOT NULL AUTO_INCREMENT,
-  `FirstName` CHAR(55) NOT NULL,
-  `LastName` CHAR(55) NOT NULL,
-  `Email` VARCHAR(255) NOT NULL,
-  `Password` VARCHAR(255) NOT NULL,
-  `IsAdmin` TINYINT(2) NOT NULL DEFAULT 0,
-  `IsDelete` TINYINT(2) NOT NULL,
+-- -----------------------------------------------------
+-- Table `SportHub`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SportHub`.`User` (
+  `UserId` VARCHAR(150) NOT NULL,
+  `Email` VARCHAR(70) NOT NULL,
+  `Password` VARCHAR(255),
+  `FirstName` VARCHAR(100) NOT NULL,
+  `LastName` VARCHAR(100) NOT NULL,
+  `IsAdmin` TINYINT NOT NULL DEFAULT '0',
+  `IsActivated` TINYINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`UserId`),
-  UNIQUE INDEX `Userid_UNIQUE` (`UserId` ASC) VISIBLE,
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE,
-  UNIQUE INDEX `Password_UNIQUE` (`Password` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE INDEX `UserId_UNIQUE` (`UserId` ASC) VISIBLE,
+  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE)
+  ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `SportHub`.`Token`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SportHub`.`Token` (
+  `RefreshToken` VARCHAR(500) NOT NULL,
+  `UserId` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`RefreshToken`),
+  UNIQUE INDEX `UserId_UNIQUE` (`UserId` ASC) VISIBLE,
+  CONSTRAINT `fk_UserId`
+  FOREIGN KEY (`UserId`)
+  REFERENCES `SportHub`.`User` (`UserId`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE)
+  ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `SportHub`.`Category` (
   `CategoryId` INT NOT NULL,
@@ -38,20 +55,20 @@ CREATE TABLE IF NOT EXISTS `SportHub`.`Articles` (
   `MainText` TEXT NOT NULL,
   `PublishingDate` DATETIME NOT NULL DEFAULT now(),
   `ImageId` INT NOT NULL,
-  `AuthorId` INT NOT NULL,
+  `AuthorId` VARCHAR(150) NOT NULL,
   `CategoryId` INT NOT NULL,
   PRIMARY KEY (`ArticleId`),
   INDEX `fk_Articles_Images_idx` (`ImageId` ASC) VISIBLE,
-  INDEX `fk_Articles_UserAccount_idx` (`AuthorId` ASC) VISIBLE,
+  INDEX `fk_Articles_User_idx` (`AuthorId` ASC) VISIBLE,
   INDEX `fk_Articles_Category_idx` (`CategoryId` ASC) VISIBLE,
   CONSTRAINT `fk_Articles_Images`
     FOREIGN KEY (`ImageId`)
     REFERENCES `SportHub`.`Images` (`ImageId`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_Articles_UserAccount`
+  CONSTRAINT `fk_Articles_User`
     FOREIGN KEY (`AuthorId`)
-    REFERENCES `SportHub`.`UserAccount` (`UserId`)
+    REFERENCES `SportHub`.`User` (`UserId`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Articles_Category`
@@ -80,13 +97,13 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `SportHub`.`Comments` (
   `CommentId` INT NOT NULL,
   `Message` VARCHAR(1000) NOT NULL,
-  `AuthorId` INT NOT NULL,
+  `AuthorId` VARCHAR(150) NOT NULL,
   PRIMARY KEY (`CommentId`),
   UNIQUE INDEX `CommentId_UNIQUE` (`CommentId` ASC) VISIBLE,
-  INDEX `fk_Comments_UserAccount_idx` (`AuthorId` ASC) VISIBLE,
-  CONSTRAINT `fk_Comments_UserAccount`
+  INDEX `fk_Comments_User_idx` (`AuthorId` ASC) VISIBLE,
+  CONSTRAINT `fk_Comments_User`
     FOREIGN KEY (`AuthorId`)
-    REFERENCES `SportHub`.`UserAccount` (`UserId`)
+    REFERENCES `SportHub`.`User` (`UserId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
