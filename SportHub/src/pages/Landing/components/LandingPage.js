7362529e97358265bbd49f5752f3deea1e6e2388
Maxim Orlianskyi <orlianskyimaxim@gmail.com>
2023-05-06 21:54:19 +0300

    Header fix

--- SportHub/src/pages/Landing/components/LandingPage.js
+++ SportHub/src/pages/Landing/components/LandingPage.js
@@ -1,23 +1,16 @@
 import Footer from "../../../modules/Footer";
-import UserHeader from "../../../modules/UserHeader/components/UserHeader";
 import styles from "../../../modules/UserHeader/style/style.module.scss";
 import SidebarLeft from "../../../modules/Sidebar-left/componets/SidebarLeft";
-import NotLoginedHeader from "../../../modules/NotLoginedHeader/componets/NotLoginedHeader";
-import AdminHeader from "../../../modules/AdminHeader";
-import {useAuthStore} from "../../../store/useAuthStore";
-import {useNavigate} from "react-router-dom";
-
+import Header from "../../../modules/Header";
 
 const LandingPage = () =>
 {
-    const { userData, setUserData} = useAuthStore()
-    const navigate = useNavigate()
     return (
         <>
+                <Header/>
             <div className={styles.container}>
-                <NotLoginedHeader></NotLoginedHeader>
                 <SidebarLeft></SidebarLeft>
-                <Footer></Footer>
+                <Footer/>
             </div>
         </>);
 }
