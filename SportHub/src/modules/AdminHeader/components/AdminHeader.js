288569b4bdac89885b66ebd660e9fc9832dfa81e
Oleh-Kr <mayorford777@gmail.com>
2023-05-16 15:58:16 +0300

    fix merge problems

--- SportHub/src/modules/AdminHeader/components/AdminHeader.js
+++ SportHub/src/modules/AdminHeader/components/AdminHeader.js
@@ -18,22 +18,22 @@ export default function AdminHeader(){
                     </div>
 
                     <div className={styles.mini_profile} onClick={() => setProfileDropdownListActive(!profileDropdownListActive)}>
+                        <div className={styles.mini_profile_wrapper}>
+                            <img className={styles.profile_picture} src="https://images.pexels.com/photos/14306688/pexels-photo-14306688.jpeg"  alt={""}/>
 
-                        <img className={styles.profile_picture} src="https://images.pexels.com/photos/14306688/pexels-photo-14306688.jpeg"  alt={""}/>
-
-                            <div className={styles.profile_info} >
-                                <div className={styles.profile_labels}>
-                                    <div className={styles.name_surname_label}>
-                                        <div>Test Testenko</div><img className={styles.open_profile_menu} src={process.env.PUBLIC_URL + '/icons/Polygon.svg'}/>
+                                <div className={styles.profile_info} >
+                                    <div className={styles.profile_labels}>
+                                        <div className={styles.name_surname_label}>
+                                            <div>Test Testenko</div>
+                                            <img className={styles.open_profile_menu} src={'/icons/Polygon.svg'}/>
+                                        </div>
+                                        <div className={styles.administrator_label}>Administrator</div>
                                     </div>
-                                    <div className={styles.administrator_label}>Administrator</div>
                                 </div>
+                                <ProfileSidebar active={profileDropdownListActive} />
                             </div>
-                            <ProfileSidebar active={profileDropdownListActive} />
                         </div>
-
                     </div>
-                </div>
             </div>
         </HeaderContainer>
     )
