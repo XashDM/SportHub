e7fc6bd79a7b20ec733e524d5192ae2fb3ef0917
Maxim Orlianskyi <orlianskyimaxim@gmail.com>
2023-05-23 19:15:04 +0300

    Add controller for users list + dto + simple unit test

---
+++ SportHub.Data/DTO/UserListResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace SportHub.Data.DTO
+{
+    public class UserListResponse
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public bool IsActivated { get; set; }
+    }
+}
---
+++ SportHub.UnitTests/UserControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SportHub.API;
+using SportHub.Business;
+using SportHub.Business.Implementations;
+using SportHub.Data.Entities;
+using SportHub.Data.Interfaces;
+using SportHub.Controllers;
+using SportHub.Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace SportHub.UnitTests
+{
+
+    // Need add more tests after finishing frontend
+    [TestFixture]
+    public class UsersControllerTests
+    {
+        private UserController _controller;
+        [Test]
+        public async Task GetUsersListAsync_ReturnsUnauthorizedWithoutAdminPolicy()
+        {
+
+            // Act
+            var result = await _controller.GetUsersListAsync();
+
+            // Assert
+            var unauthorizedResult = result as UnauthorizedResult;
+            Assert.IsNotNull(unauthorizedResult);
+        }
+
+    }
+}
