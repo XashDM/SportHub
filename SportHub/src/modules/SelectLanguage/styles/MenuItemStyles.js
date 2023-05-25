3c7cb733ebe1c28c36461149fd3936e0c490af52
OndoriSp <ondori_special@ukr.net>
2023-05-25 09:47:55 +0300

    Fixed styles for MenuItemStyles in language selector

--- SportHub/src/modules/SelectLanguage/styles/MenuItemStyles.js
+++ SportHub/src/modules/SelectLanguage/styles/MenuItemStyles.js
@@ -2,15 +2,24 @@ import { MenuItem, styled } from "@mui/material"
 
 const MenuItemStyles = styled(MenuItem)({
     fontFamily: "var(--font-style-open-sans)",
+    color: "var(--color-dark-text-color)",
     "&:hover": {
         backgroundColor: "var(--color-red-regular-transparent)",
         color: "var(--color-red-regular)",
         cursor: "pointer",
     },
-    "&:selected": {
+    "&.Mui-selected": {
         backgroundColor: "var(--color-red-regular-transparent)",
         color: "var(--color-red-regular)",
-      },
+        "&.Mui-focusVisible": {
+            background: "var(--color-red-regular-transparent)"
+        },
+        "&:hover": {
+            backgroundColor: "var(--color-red-regular-transparent)",
+            color: "var(--color-red-regular)",
+            cursor: "pointer",
+        },
+    }
 })
 
 export default MenuItemStyles
