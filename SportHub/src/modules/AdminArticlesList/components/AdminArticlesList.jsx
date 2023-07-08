76bf9205e5ca4ad62286dba1ddf3031082c4188f
Oleh-Kril <oleh.kril.t@gmail.com>
2023-07-08 15:42:21 +0300

    fix loading articles when switching category tabs

--- SportHub/src/modules/AdminArticlesList/components/AdminArticlesList.jsx
+++ SportHub/src/modules/AdminArticlesList/components/AdminArticlesList.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react"
-import {useAtom} from "jotai";
+import {useAtom} from "jotai"
 import InfiniteScroll from "react-infinite-scroll-component"
 import {useTranslation} from "react-i18next"
 
@@ -9,7 +9,7 @@ import AutoComplete from "../../../ui/AutoComplete"
 import getPageOfArticlesRequest from "../helpers/getPageOfArticlesRequest"
 import ArticleMenu from "../../ArticleMenu"
 import {PAGE_CONSTANTS} from "../../../constants/PageConstants"
-import {adminMenuState} from "../../../store/states/adminMenuState";
+import {adminMenuState} from "../../../store/states/adminMenuState"
 
 function AdminArticlesList() {
     const [adminMenu, setAdminMenu] = useAtom(adminMenuState)
@@ -22,6 +22,7 @@ function AdminArticlesList() {
     const [isLastArticleHaveBeenFetched, setIsLastArticleHaveBeenFetched] = useState(false)
     const [currentPageNumber, setCurrentPageNumber] = useState(PAGE_CONSTANTS.FIRST_PAGE_NUMBER)
     const { i18n } = useTranslation()
+    const [shouldFetchData, setShouldFetchData] = useState(false)
 
     async function fetchData() {
         if (isLoading) return
@@ -65,15 +66,20 @@ function AdminArticlesList() {
     useEffect(() => {
         setArticles([])
         setCurrentPageNumber(PAGE_CONSTANTS.FIRST_PAGE_NUMBER)
+        setShouldFetchData(true)
     }, [category])
     useEffect(() => {
         if(articles.length !== 0){
             setCurrentPageNumber((prevPageNumber) => prevPageNumber+1)
+            setShouldFetchData(true)
         }
     }, [articles])
     useEffect(() => {
-        fetchData()
-    }, [currentPageNumber])
+        if(shouldFetchData){
+            fetchData()
+            setShouldFetchData(false)
+        }
+    }, [shouldFetchData])
 
     function convertArticlesToCards(){
         return articles.map((article, idx) => (
