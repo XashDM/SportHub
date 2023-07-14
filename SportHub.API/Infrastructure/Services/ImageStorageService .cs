71105cd479289effff4c458061b6b3d53f8f5972
malyna2 <malynovskyiandriy35@gmail.com>
2023-07-14 19:44:10 +0300

    backend

--- SportHub.API/Infrastructure/Services/ImageStorageService .cs
+++ SportHub.API/Infrastructure/Services/ImageStorageService .cs
@@ -1,21 +1,29 @@
 ﻿using SportHub.API.Infrastructure.Interfaces;
+using Azure.Storage.Blobs;
 
 namespace SportHub.API.Infrastructure.Services
 {
 	public class ImageStorageService : IImageStorageService
 	{
+		private readonly string _blobStorageConnectionString;
+		private readonly string _blobContainerName;
+
+		public ImageStorageService(IConfiguration config)
+		{
+			_blobStorageConnectionString = config.GetSection("Azure")["BlobStorageConnectionString"];
+			_blobContainerName = config.GetSection("Azure")["BlobStorageContainerName"];
+		}
+
 		public async Task<string> SaveImageFile(IFormFile file)
 		{
 			var imageId = Guid.NewGuid().ToString();
 
 			var fileName = imageId + Path.GetExtension(file.FileName);
-			var parentPath = Directory.GetParent(Directory.GetCurrentDirectory());
-			var filePath = Path.Combine(parentPath.FullName, "SportHub\\public\\saved_images", fileName);
 
-			using (var stream = new FileStream(filePath, FileMode.Create))
-			{
-				await file.CopyToAsync(stream);
-			}
+			var container = new BlobContainerClient(_blobStorageConnectionString, _blobContainerName);
+			var blob = container.GetBlobClient(fileName);
+
+			await blob.UploadAsync(file.OpenReadStream());
 
 			return fileName;
 		}
