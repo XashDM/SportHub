c78102bbf7a67bd9876a51e03d53d9b5bd1850f3
Adeodonne <bbozhik284@gmail.com>
2023-07-17 20:51:14 +0300

    add redirection to article view when you click on breakdowns

--- SportHub/src/modules/UserBreakdownsSection/components/Breakdown/components/SmallArticleVisualisation/components/SmallArticleVisualisation.js
+++ SportHub/src/modules/UserBreakdownsSection/components/Breakdown/components/SmallArticleVisualisation/components/SmallArticleVisualisation.js
@@ -1,10 +1,14 @@
 import styles from "../styles/styles.module.scss"
 import {ReactSVG} from "react-svg"
+import {useNavigate} from "react-router-dom"
+import {ROUTES} from "../../../../../../../routes/routes"
 
 export default function SmallArticleVisualisation({article}){
 
+    const navigate = useNavigate()
+
     return(<div>
-        <div className={styles.content}>
+        <div className={styles.content} onClick={() => {navigate(ROUTES.ARTICLE.replace(':articleId', article.articleId))}}>
             <img className={styles.image} src={article.image.url} />
             <div className={styles.text}>
                 <div className={styles.main_text}>{article.title}</div>
