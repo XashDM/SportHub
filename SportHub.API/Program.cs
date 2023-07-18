938690fba126b6fc422c781d8fe47e0535c258b2
Oleh-Kril <91199925+Oleh-Kril@users.noreply.github.com>
2023-07-18 07:53:52 +0300

    fix program.cs tabulation (#74)

--- SportHub.API/Program.cs
+++ SportHub.API/Program.cs
@@ -19,114 +19,109 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAutoMapper(typeof(MapperConfig));
+        builder.Services.AddAutoMapper(typeof(MapperConfig));
 
-// Add services to the container.
+        // Add services to the container.
+        
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
+        builder.Services.AddScoped<ILocationRepository, LocationRepository>();
+        builder.Services.AddScoped<ILocationService, LocationService>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddScoped<IImageRepository, ImageRepository>();
+        builder.Services.AddScoped<IImageService, ImageService>();
 
-builder.Services.AddScoped<ILocationRepository, LocationRepository>();
-builder.Services.AddScoped<ILocationService, LocationService>();
+        builder.Services.AddScoped<INavigationRepository, NavigationRepository>();
+        builder.Services.AddScoped<INavigationService, NavigationService>();
 
-builder.Services.AddScoped<IImageRepository, ImageRepository>();
-builder.Services.AddScoped<IImageService, ImageService>();
+        builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
+        builder.Services.AddScoped<IArticleService, ArticleService>();
 
-builder.Services.AddScoped<INavigationRepository, NavigationRepository>();
-builder.Services.AddScoped<INavigationService, NavigationService>();
+        builder.Services.AddScoped<IImageStorageService, ImageStorageService>();
 
-builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
-builder.Services.AddScoped<IArticleService, ArticleService>();
+        builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
 
-builder.Services.AddScoped<IImageStorageService, ImageStorageService>();
+        builder.Services.AddScoped<ISubCategoryRepository, SubCategoryRepository>();
+        builder.Services.AddScoped<ISubCategoryService, SubCategoryService>();
 
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
+        builder.Services.AddScoped<ITeamsRepository, TeamsRepository>();
+        builder.Services.AddScoped<ITeamsService, TeamsService>();
 
-builder.Services.AddScoped<ISubCategoryRepository, SubCategoryRepository>();
-builder.Services.AddScoped<ISubCategoryService, SubCategoryService>();
+        builder.Services.AddScoped<IBreakDownRepository, BreakDownRepository>();
+        builder.Services.AddScoped<IBreakDownService, BreakDownService>();
 
-builder.Services.AddScoped<ITeamsRepository, TeamsRepository>();
-builder.Services.AddScoped<ITeamsService, TeamsService>();
+        builder.Services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
 
-builder.Services.AddScoped<INavigationTreeRepository, NavigationTreeRepository>();
-builder.Services.AddScoped<INavigationTreeService,NavigationTreeService>();
+        builder.Services.AddScoped<IEmailService, EmailService>();
+        builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<ILanguageRepository, LanguageRepository>();
 
-builder.Services.AddScoped<IBreakDownRepository, BreakDownRepository>();
-builder.Services.AddScoped<IBreakDownService, BreakDownService>();
+        builder.Services.AddScoped<IJwtService, JwtService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<ILanguageService, LanguageService>();
 
-builder.Services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
+        // Add jwt access token validation
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(o =>
+        {
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"])),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminPolicy", policy =>
+            {
+                policy.RequireRole("admin");
+            });
 
-builder.Services.AddScoped<IEmailService, EmailService>();
-builder.Services.AddScoped<ITokenRepository, TokenRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ILanguageRepository, LanguageRepository>();
+            options.AddPolicy("UserPolicy", policy =>
+            {
+                policy.RequireRole("user");
+            });
+        });
 
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ILanguageService, LanguageService>();
-
-// Add jwt access token validation
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey
-            (Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"])),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ClockSkew = TimeSpan.Zero
-    };
-});
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("AdminPolicy", policy =>
-    {
-        policy.RequireRole("admin");
-    });
-
-    options.AddPolicy("UserPolicy", policy =>
-    {
-        policy.RequireRole("user");
-    });
-});
-
-builder.Services.AddHttpClient();
-var app = builder.Build();
+        builder.Services.AddHttpClient();
+        var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+	        app.UseSwagger();
+	        app.UseSwaggerUI();
+        }
 
-app.UseCors(builder =>
-{
-    builder.AllowAnyOrigin()
-           .AllowAnyMethod()
-           .AllowAnyHeader();
-    
-});
-app.UseHttpsRedirection();
+        app.UseCors(builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+            
+        });
+        app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
 
-app.Run();
-
+        app.Run();
     }
 }
